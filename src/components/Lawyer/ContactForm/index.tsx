e7113d8ade4bd06707a96e902adedba8ddec3f60
Dhruba Datta <dhrubadattaanjan@gmail.com>
2026-02-03 12:27:51 +0600

    Form redirection fixed!

--- src/components/Lawyer/ContactForm/index.tsx
+++ src/components/Lawyer/ContactForm/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ContactForm from '../../Common/ContactForm';
 
 const ContactFormPage: React.FC = () => {
-  return <ContactForm successRedirect="/marketing-for-law-firm/thank-you-cal" />;
+  return <ContactForm />;
 };
 
 export default ContactFormPage;
